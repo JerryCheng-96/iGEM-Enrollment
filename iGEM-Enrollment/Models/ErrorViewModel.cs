26428de2a5ac1735cf2c00f495321a2a196ab457
JerryCheng <jerrycheng96@live.com>
2017-11-19 20:12:08 +0800

    Add project files.

---
+++ iGEM-Enrollment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iGEM_Enrollment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
